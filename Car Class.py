df688e22f45055530c409f5785ebdcf8981ea2a7
Matt42200 <mranderson4@students.unwsp.edu>
2025-04-04 23:23:08 -0500

    Add files via upload

---
+++ Car Class.py
@@ -0,0 +1,35 @@
+﻿class Car:
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0  # Initial speed is 0
+
+
+    def accelerate(self):
+        """Increases the car's speed by 5."""
+        self.__speed += 5
+
+
+    def brake(self):
+        """Decreases the car's speed by 5."""
+        self.__speed -= 5
+
+
+    def get_speed(self):
+        """Returns the current speed of the car."""
+        return self.__speed
+
+
+
+
+my_car = Car(2021, "Toyota")
+
+
+for _ in range(5):
+    my_car.accelerate()
+    print(f"Current speed after accelerating: {my_car.get_speed()} mph")
+
+
+for _ in range(5):
+    my_car.brake()
+    print(f"Current speed after braking: {my_car.get_speed()} mph")
---
+++ Flow chart.py
@@ -0,0 +1,32 @@
+﻿+------------------------+
+|     Start (Car Class)   |
++------------------------+
+           |
+           v
++----------------------------+
+|   Define __init__ method   |
+|  - Initialize year_model   |
+|  - Initialize make          |
+|  - Initialize __speed = 0  |
++----------------------------+
+           |
+           v
++----------------------------+
+| Define accelerate method   |
+|  - Add 5 to __speed        |
++----------------------------+
+           |
+           v
++----------------------------+
+| Define brake method        |
+|  - Subtract 5 from __speed |
++----------------------------+
+           |
+           v
++----------------------------+
+| Define get_speed method    |
+|  - Return __speed          |
++----------------------------+
+           |
+           v
+        End (Car Class)
